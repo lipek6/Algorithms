42c3976b534d5a6a1a38b673b3d24fd2e7a0dabb
fald <felipeaugustolira2006@gmail.com>
2026-03-13 08:06:45 -0300

    Apressqado aaa,done

---
+++ Lab2/1_Pokemons/alg.cpp
@@ -0,0 +1,60 @@
+/*
+    Requirements:
+    1. Insert an element
+    2. Find an element by its name
+    3. Remove element by its name
+    4. Print all elements
+
+    Restrictions:
+    1. Use a double linked list
+*/
+
+/*
+    Solution idea: 
+    - Pokedex is the system class that will hold all pokemons and have the interface to access them.
+    - The data inside the pokedex is gonna be consistent of a double linked list, first/last element pointer and size.
+    - Pokedexs double linked list is gonna use nodes that can point two the next and last node. Nodes can have a pokemon as internal data. 
+*/
+
+
+
+#include <iostream> 
+
+struct Pokemon
+{
+    std::string name;
+    int age;
+};
+
+typedef 
+struct Node
+{
+
+};
+
+
+
+class Pokedex
+{
+private:
+
+
+
+public:
+
+
+};
+
+
+
+
+
+
+int main(void)
+{
+
+
+
+
+
+}
