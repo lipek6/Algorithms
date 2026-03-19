bbf418f177e12da56315c5954d6d6f0f69c6acd5
fald <felipeaugustolira2006@gmail.com>
2026-03-19 18:48:25 -0300

    Finished the single linked list refactoring.

--- AlgsLib/DataStructures/SingleLinkedList.h
+++ AlgsLib/DataStructures/SingleLinkedList.h
@@ -3,7 +3,7 @@
 
     What is this?
 	~~~~~~~~~~~~~
-    This code implements a single linked list.
+    This code implements a single linked list that can hold any kind of data/object.
 */
 
 #include <stdexcept>
@@ -15,19 +15,32 @@ class SingleLinkedList
 private:
     struct Node
     {
-        Node* next;
         T data;
+        Node* next;
 
-        Node() : next(nullptr) {}
         Node(const T& data, Node* next = nullptr) : data(data), next(next) {}
         ~Node() {}
     };
+  
     Node* begin;
     Node* end;
     size_t listSize;
 
 public:
+    // ==========================================
+    // CONSTRUCTORS & DESTRUCTORS 
+    // ==========================================
+
+    // Default constructor
     SingleLinkedList() : begin(nullptr), end(nullptr), listSize(0) {}
+    
+    // Default destructor
+    ~SingleLinkedList()
+    {
+        clear();
+    }
+
+    // Deep copy constructor
     SingleLinkedList(const SingleLinkedList& source) : begin(nullptr), end(nullptr), listSize(0)
     {
         if(source.begin == nullptr) return;
@@ -39,8 +52,9 @@ public:
             auxPtr = auxPtr->next;
         }
     }
-
-    SingleLinkedList& operator=(const SingleLinkedList& source)    // Remember that this func must return SingleLinkedList&, so we can do chain assignments and don't lose time making shallow useless copys
+    
+    // Copy assignment operator
+    SingleLinkedList& operator=(const SingleLinkedList& source)
     {
         if(this == &source) return *this;
 
@@ -55,6 +69,135 @@ public:
         return *this;
     }
 
+    // Move constructor
+    SingleLinkedList(SingleLinkedList&& source) noexcept : begin(source.begin), end(source.end), listSize(source.listSize)
+    {
+        source.begin = nullptr;
+        source.end = nullptr;
+        source.listSize = 0;
+    }
+
+    // Move assignment operator
+    SingleLinkedList& operator=(SingleLinkedList&& source) noexcept
+    {
+        if(this == &source) return *this;
+
+        this->clear();
+
+        begin = source.begin;
+        end = source.end;
+        listSize = source.listSize;
+
+        source.begin = nullptr;
+        source.end = nullptr;
+        source.listSize = 0;
+
+        return *this;
+    }
+
+    // ==========================================
+    // CAPACITY  
+    // ==========================================
+
+    // Checks if the list is empty
+    bool empty() const
+    {
+        return listSize == 0;
+    }
+
+    // Returns the number of nodes on list
+    size_t size() const
+    {
+        return listSize;
+    }
+
+    // ==========================================
+    // GETTERS
+    // ==========================================
+
+    // Returns the first element of the list
+    T& front()
+    {
+        if(begin != nullptr)
+            return begin->data;
+        throw std::out_of_range("Empty list");
+    }
+
+    const T& front() const
+    {
+        if(begin != nullptr)
+            return begin->data;
+        throw std::out_of_range("Empty list");
+    }
+
+    // Returns the last element of the list
+    T& back()
+    {
+        if(end != nullptr)
+            return end->data;
+        throw std::out_of_range("Empty list");
+    }
+
+    const T& back() const
+    {
+        if(end != nullptr)
+            return end->data;
+        throw std::out_of_range("Empty list");
+    }
+
+    // Index operator
+    T& operator[](const size_t pos) 
+    {
+        if(pos >= listSize) throw std::out_of_range("Index out of range");
+        
+        if(pos == listSize - 1) return end->data;
+
+        Node* auxPtr = begin;        
+        for(size_t i = 0; i < pos; i++)
+        {
+            auxPtr = auxPtr->next;
+        }        
+        return auxPtr->data;
+    }
+
+    const T& operator[](const size_t pos) const
+    {
+        if(pos >= listSize) throw std::out_of_range("Index out of range");
+        
+        if(pos == listSize - 1) return end->data;
+
+        Node* auxPtr = begin;        
+        for(size_t i = 0; i < pos; i++)
+        {
+            auxPtr = auxPtr->next;
+        }        
+        return auxPtr->data;
+    }
+
+    // Linear search
+    std::optional<size_t> find(const T &target) const
+    {
+        if(listSize == 0) return std::nullopt;
+
+        size_t pos = 0;
+        Node* auxPtr = begin;
+
+        while(pos < listSize && auxPtr->data != target)
+        {
+            auxPtr = auxPtr->next;
+            pos++;
+        }
+
+        if(pos < listSize) 
+            return pos;
+        return std::nullopt;
+    }
+
+    // ==========================================
+    // MODIFIERS
+    // ==========================================
+
+    // Removes all the nodes of the list
     void clear()
     {
         if(begin == nullptr) return;
@@ -72,26 +215,11 @@ public:
         listSize = 0;
     }
 
-    ~SingleLinkedList()
-    {
-        clear();
-    }
-
-    bool empty() const
-    {
-        if(listSize == 0)
-            return true;
-        else
-            return false;
-    }
-
-    size_t size() const
-    {
-        return listSize;
-    }
-
+    // Creates a node with the given data at the given position
     void insert(const T &newData, const size_t pos)
     {
+        if(pos > listSize) throw std::out_of_range("Index out of range");
+
         if(pos == 0)
         {
             begin = new Node(newData, begin);
@@ -114,6 +242,7 @@ public:
         listSize++;
     }
 
+    // Deletes the node at the given position
     void remove(const size_t pos)
     {
         if(pos >= listSize) throw std::out_of_range("Index out of range");
@@ -139,97 +268,26 @@ public:
 
             delete auxPtr2;
         }  
-
         listSize--;
     }
 
-    void push_back(const T &newData)
-    {
-        insert(newData, listSize);
-    }
+    // Creates a node with the given data at the end of the list
+    void push_back(const T &newData) { insert(newData, listSize); }
 
-    void push_front(const T &newData)
-    {
-        insert(newData, 0);
-    }
+    // Creates a node with the given data at the begin of the list
+    void push_front(const T &newData) { insert(newData, 0); }
+    
+    // Deletes the last node of the list
+    void pop_back(){ remove(listSize - 1); }
 
-    T& front()
-    {
-        if(begin != nullptr)
-            return begin->data;
-        else
-            throw std::out_of_range("Empty list");
-    }
+    // Deletes the first node of the list
+    void pop_front() { remove(0); }
 
-    const T& front() const
-    {
-        if(begin != nullptr)
-            return begin->data;
-        else
-            throw std::out_of_range("Empty list");
-    }
+    // ==========================================
+    // OVERLOADINGS
+    // ==========================================
 
-    T& back()
-    {
-        if(end != nullptr)
-            return end->data;
-        else
-            throw std::out_of_range("Empty list");
-    }
-
-    const T& back() const
-    {
-        if(end != nullptr)
-            return end->data;
-        else
-            throw std::out_of_range("Empty list");
-    }
-
-    void pop_back()
-    {
-        remove(listSize-1);
-    }
-
-    void pop_front()
-    {
-        remove(0);
-    }
-
-    std::optional<size_t> find(const T &target) const
-    {
-        if(listSize == 0) return std::nullopt;
-
-        size_t pos = 0;
-        Node* auxPtr = begin;
-
-        while(pos < listSize && auxPtr->data != target)
-        {
-            auxPtr = auxPtr->next;
-            pos++;
-        }
-
-        if(pos < listSize) 
-            return pos;
-        else
-            return std::nullopt;
-    }
-
-    // TODO: Add a const version of this guy below 
-    T& operator[](const size_t pos) 
-    {
-        if(pos >= listSize) throw std::out_of_range("Index out of range");
-        
-        if(pos == listSize - 1) return end->data;
-
-        Node* auxPtr = begin;        
-        for(size_t i = 0; i < pos; i++)
-        {
-            auxPtr = auxPtr->next;
-        }        
-        return auxPtr->data;
-    }
-
-    // TODO: Take a look at move semantics "&&" to optimize this 
+    // Adds the nodes of the rigth list to the end of the left list
     SingleLinkedList& operator+=(const SingleLinkedList& newNodes)
     {
         Node *auxPtr = newNodes.begin;
@@ -241,7 +299,7 @@ public:
         return *this;
     }
 
-    // TODO: Take a look at move semantics "&&" to optimize this 
+    // Adds the nodes of the rigth list to the end of the left list
     SingleLinkedList operator+(const SingleLinkedList& newNodes) const
     {
         SingleLinkedList<T> newList;
@@ -250,4 +308,3 @@ public:
         return newList;
     }
 };
-
