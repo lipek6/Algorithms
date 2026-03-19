a26b7d3e341bdfd142cd041959a2da9ae0def9c8
fald <felipeaugustolira2006@gmail.com>
2026-03-19 20:23:34 -0300

    Fix: const correctness into the queue and the stack.

--- AlgsLib/DataStructures/Queue.h
+++ AlgsLib/DataStructures/Queue.h
@@ -7,28 +7,15 @@ private:
     SingleLinkedList<T> list;    
 
 public:
-    void push(const T& newData)
-    {
-        list.push_back(newData);
-    }
+    void push(const T& newData) { list.push_back(newData); }
+    void pop() { list.pop_front(); }
 
-    void pop()
-    {
-        list.pop_front();
-    }
+    T& front() { return list.front(); }
+    T& back() { return list.back(); }
 
-    T& front() const
-    {
-        return list.front();
-    }
+    const T& front() const { return list.front(); }
+    const T& back() const { return list.back(); }
 
-    T& back() const
-    {
-        return list.back();
-    }
-
-    size_t size() const
-    {
-        return list.size();
-    }
+    size_t size() const { return list.size(); }
+    bool empty() const { return list.empty(); }
 };
--- AlgsLib/DataStructures/Stack.h
+++ AlgsLib/DataStructures/Stack.h
@@ -7,23 +7,12 @@ private:
     SingleLinkedList<T> list;
 
 public:
-    void pop()
-    {
-        list.pop_front();
-    }
+    void pop() { list.pop_front(); }
+    void push(const T& newData) { list.push_front(newData); }
 
-    void push(const T& newData)
-    {
-        list.push_front(newData);
-    }
+    T& top() { return list.front(); }
+    const T& top() const { return list.front(); }
 
-    T& top() const
-    {
-        return list.front();
-    }
-
-    size_t size() const
-    {
-        return list.size();
-    }
+    size_t size() const { return list.size(); }
+    bool empty() const { return list.empty(); }
 };
