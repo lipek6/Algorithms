929a36511a452f91752b2184d11b0b9dd459db15
fald <felipeaugustolira2006@gmail.com>
2026-03-27 17:08:40 -0300

    almost done bst.

--- AlgsLib/DataStructures/Includes/BinaryTree.cpp
+++ AlgsLib/DataStructures/Includes/BinaryTree.cpp
@@ -20,7 +20,80 @@ private:
 
     Node* root;
     size_t numNodes;
-    size_t height;
+
+    Node* searchNode(const K& targetKey)
+    {
+        Node* auxPtr1 = root;
+        while(auxPtr1 != nullptr)
+        {
+            if(targetKey < auxPtr1->key)
+                auxPtr1 = auxPtr1->left;
+            else if(targetKey > auxPtr1->key)
+                auxPtr1 = auxPtr1->right;
+            else
+                break;
+        }
+        return auxPtr1;
+    }
+
+    Node* minimum(Node* subtreeRoot = root)
+    {
+        if(subtreeRoot == nullptr) 
+            return nullptr;
+            
+        while(subtreeRoot->left != nullptr)
+        {
+            subtreeRoot = x->left;
+        }
+        return subtreeRoot;
+    }
+
+    Node* maximum(Node* subtreeRoot = root)
+    {
+        if(subtreeRoot == nullptr) 
+            return nullptr;
+
+        while(subtreeRoot->right != nullptr)
+        {
+            subtreeRoot = subtreeRoot->right;
+        }
+        return subtreeRoot;
+    }
+
+    Node* successor(Node* node)
+    {
+        if(node == nullptr) 
+            return nullptr;
+
+        if(node->right != nullptr)
+            return minimum(node->right);
+
+        Node* auxPtr1 = node->parent;
+        while(auxPtr1 != nullptr && auxPtr1->right == node)
+        {
+            node = auxPtr1;
+            auxPtr1 = auxPtr1->parent
+        }
+        return auxPtr1;
+    }
+
+    Node* predecessor(Node* node)
+    {
+        if(node == nullptr)
+            return nullptr;
+
+        if(node->left != nullptr)
+            return maximum(node->left);
+
+        Node* auxPtr1 = node->parent;
+        while(auxPtr1 != nullptr && auxPtr1->left == node)
+        {
+            node = auxPtr1;
+            auxPtr1 = auxPtr1->parent;
+        }        
+        return auxPtr1;
+    }
+
 
     void transplant(){}
 
@@ -61,18 +134,55 @@ public:
 
     D* search(const K& targetKey)
     {
-        Node* auxPtr1 = root;
-        Node* auxPtr2 = root;
-
-        while(auxPtr1 != nullptr)
-        {
-            auxPtr2 = auxPtr1;
-            if(targetKey < auxPtr1->key)
-                auxPtr1 = auxPtr1->left;
-            else if()
-                auxPtr1 = auxPtr1->right;
-        }
+        Node* targetNode = searchNode(targetKey);
+        
+        if(targetNode != nullptr)
+            return &(targetNode->data);
+        else
+            return nullptr;
     }
+
+    D* minimum(const K& subtreeRootKey)
+    {
+        Node* targetNode = searchNode(subtreeRootKey);
+        if(targetNode == nullptr) return nullptr;
+        
+        Node* minNode = minimum(targetNode);
+        if(minNode == nullptr) return nullptr;
+        
+        return &(minNode->data);
+    }
+
+    D* maximum(const K& subtreeRootKey)
+    {
+        return &(maximum(searchNode(subtreeRootKey))->data);
+    }
+
+    D* successor(const K& node)
+    {
+        return &(successor(searchNode(node))->data);
+    }
+
+    D* predecessor(const K& node)
+    {
+        Node* targetNode = searchNode(node);
+        if(targetNode == nullptr) return nullptr;
+
+        Node* predNode = predecessor(targetNode);
+        if(predNode == nullptr) return nullptr;
+
+        return &(predNode->data);
+    }
+
+    void printInOrder(std::ostream& stream, Node* subtreeRoot)
+    {
+        if(subtreeRoot == nullptr) return;
+
+        printInOrder(stream, subtreeRoot->left);
+        stream << subtreeRoot;
+        printInOrder(stream, subtreeRoot->right);
+    }
+
 
 
     void remove(const T& delData)
